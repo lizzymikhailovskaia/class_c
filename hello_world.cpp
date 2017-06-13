daf5b0a61527b3ce8c7f87667aac38c8881ba673
Lizzy <0572788@gmail.com>
2017-06-13 10:39:41 -0700

    my first program on c++

---
+++ hello_world.cpp
@@ -0,0 +1,15 @@
+
+    /* Lizzy Mikhailovskaia
+       hello.cpp
+
+       Simple Hello World program
+    */
+
+    #include <iostream>
+    using namespace std;
+
+    int main()
+    {
+      cout << "Hello, World!\n"; // Display the string.
+      return 0;
+    }
