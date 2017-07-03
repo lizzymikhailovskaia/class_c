2165e20b07f1699eca982d5eca1722fdc8a66507
Lizzy <0572788@gmail.com>
2017-07-03 10:32:03 -0700

    while  ex_work on c++

---
+++ ex_for_classc/ex_while_loop.cpp
@@ -0,0 +1,17 @@
+// This program denonstrate a simple while loop.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int number = 1;
+
+  while (number <= 5)
+  {
+    cout << "Hello\n";
+    number ++;
+  }
+  cout << "That's all!\n";
+  return 0;
+}
